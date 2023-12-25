014d246ee8305af361716a1a9c5217ee5397420e
go-dev <awwalcodestar@gmail.com>
2023-12-25 21:19:52 +0100

    initial implementation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *App) homeHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	// 	params := httprouter.ParamsFromContext(r.Context())
+
+	// 	id, err := strconv.Atoi(params.ByName("mode"))
+	// 	if err != nil {
+	// 		app.clientErr(w, err, http.StatusBadRequest)
+	// 		return
+	// 	}
+	fmt.Fprint(w, "Fetch on the data needed to implement home page of the portfolio")
+}
