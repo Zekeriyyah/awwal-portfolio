42bf8efffcb9bd13c0d50899a5abcc484182f9a9
go-dev <awwalcodestar@gmail.com>
2023-12-25 21:19:02 +0100

    handle client and server error

--- cmd/web/errors.go
+++ cmd/web/errors.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *App) clientErr(w http.ResponseWriter, err error, c int) {
+	app.errorLog.Println(err)
+	fmt.Fprint(w, c, http.StatusText(c))
+}
