e71e4c4409ec8b0ecd939d0c50d53091d21acce6
go-dev <awwalcodestar@gmail.com>
2023-12-25 21:21:39 +0100

    Initial implementation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func (app *App) Route() http.Handler {
-	return nil
+	router := httprouter.New()
+
+	router.GET("/api/home/:mode", app.homeHandler)
+	// 	// router.GET("/api/about/:mode", app.aboutHandler)
+	// 	// router.GET("/api/projects/:mode", app.getProjects)
+	// 	// router.GET("/api/projects/:mode", app.getProjectsByFilter)
+
+	return router
 }
