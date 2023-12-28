29c4640da3fbd454ed9e98f42707952f8b686cbf
go-dev <awwalcodestar@gmail.com>
2023-12-28 23:26:48 +0100

    .

--- internal/models/contact.go
+++ internal/models/contact.go
@@ -1,1 +1,18 @@
 package models
+
+import "time"
+
+type Contact struct {
+	ID         int
+	Subject    string
+	Name       string
+	Email      string
+	Phone      string
+	Address    string
+	SetMode    string
+	Created_At time.Time
+}
+
+func (c *Contact) NewContact() *Contact {
+	return &Contact{}
+}
